2a69860495f4b76e26fbf11b27ab5ddc0e11d887
Yunjie Shi <379739073@qq.com>
2018-04-25 16:17:02 -0400

    HS

--- OpticalFlowModel.py
+++ OpticalFlowModel.py
@@ -23,8 +23,23 @@ class OpticalFlowModel:
         new = cv2.Laplacian(new, cv2.CV_64F)#CV_64F为图像深度
         Ix = cv2.Sobel(new,cv2.CV_64F,1,0,ksize=3)#1，0参数表示在x方向求一阶导数
         Iy = cv2.Sobel(new,cv2.CV_64F,0,1,ksize=3)#0,1参数表示在y方向求一阶导数 
+        lamb = 10
         row = newImage.shape[0]
         col = newImage.shape[1]
+        u = np.zeros_like(newImage, dtype='float64')
+        v = np.zeros_like(newImage, dtype='float64')
+        
+        for n in range(15):
+            for i in range(1, row-1):
+                for j in range(1, col-1):
+                    _u = (u[i-1, j]+u[i+1, j]+u[i, j-1]+u[i, j+1]) * 0.25
+                    _v = (v[i-1, j]+v[i+1, j]+v[i, j-1]+v[i, j+1]) * 0.25
+                    alpha = (Ix[i, j] * _u + Iy[i, j] * _v + It[i, j]) / (1 + lamb*(Ix[i, j] ** 2 + Iy[i, j] ** 2)) * lamb
+                    u[i, j] = _u - alpha * Ix[i, j]
+                    v[i, j] = _v - alpha * Iy[i, j]
+                    if (u[i, j] ** 2 + v[i, j] ** 2) ** 0.5 > 1:
+                        newImage[i][j] = 0
+        '''
         for i in range(k, row - k):
             for j in range(k, col - k):
                 A = self.buildA(Ix, Iy, i, j, windowsSize)
@@ -32,12 +47,13 @@ class OpticalFlowModel:
                 if np.linalg.det((A.T).dot(A)) != 0:
                     Vpt = np.matrix((A.T).dot(A)).I.dot(A.T).dot(b)
                     if (Vpt[0,0]**2 + Vpt[0,1]**2)**0.5 > 10:
-                        newImage[i][j] = 0  
+                        newImage[i][j] = 0 
+                       ''' 
         return newImage
 
     def buildA(self, Ix, Iy, x, y, kernelSize):
         #build a kernel containing pixel intensities
-        mean = kernelSize//2
+        mean = kernelSize // 2
         count = 0
        #home = img[centerX, centerY] #storing the intensity of the center pixel
         A = np.zeros([kernelSize**2, 2])
@@ -69,6 +85,8 @@ if __name__=="__main__":
     # Check if camera opened successfully
     if (cap.isOpened()== False): 
         print("Error opening video stream or file")
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (160,90))
      
     # Read until video is completed
     ret, old = cap.read()
@@ -79,8 +97,9 @@ if __name__=="__main__":
         ret, new = cap.read()   
         if ret == True:
             new = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
-            #opticalImage = model.getOptical(new, old)
-            misc.imsave('./res/res' + str(count) + '.bmp', new)
+            opticalImage = model.getOptical(new, old)
+            out.write(opticalImage)
+            misc.imsave('./res/res' + str(count) + '.bmp', opticalImage)
             count += 1
         else:
             break
